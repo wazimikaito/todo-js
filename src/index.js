e25dc5440d45028fe50ff278f5a0e9ff89da4d49
wazimikaito <70002822+wazimikaito@users.noreply.github.com>
2022-04-29 14:15:54 +0900

    add delete-button function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // textboxの値を取得し，初期化する．
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // liタグを生成
+  const li = document.createElement("li");
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  p.className = "job-name";
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+
+  // add complete button
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "complete";
+  completeButton.addEventListener("click", () => {
+    alert("complete");
+  });
+
+  console.log(completeButton);
+
+  // add delete button
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+  deleteButton.addEventListener("click", () => {
+    // after pushing, remove tag
+    const deleteTarget = deleteButton.parentElement.parentElement;
+
+    document.getElementById("uncompleted-list").removeChild(deleteTarget);
+  });
+
+  console.log(deleteButton);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // add node under <ul>
+  document.getElementById("uncompleted-list").appendChild(li);
+  console.log(document.getElementById("uncompleted-list"));
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
